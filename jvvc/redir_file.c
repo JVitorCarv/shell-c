4c82fdb10b484b0621d3d5fbcdb1b60a8c12b627
Vitor <vitor.velozo2003@gmail.com>
2022-09-27 22:59:41 -0300

    Refactor and redir_file removal

--- jvvc/redir_file.c
+++ jvvc/redir_file.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+/* This file was used during the development of 
+redirections to an executable. */
 int main(int argc, char *argv[]) {
     printf("ARGC: %d\n", argc);
     for (int i = 0; i < argc; i++) {
